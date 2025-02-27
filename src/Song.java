a32ab65a414667566a22c608a3c0266709635cf2
Natg325 <nataliegrubb325@gmail.com>
2025-02-26 18:13:39 -0700

    Completed Song class and Album class

---
+++ src/Song.java
@@ -0,0 +1,69 @@
+
+public class Song {
+	private String title;
+	private String artist;
+	private String album;
+	private String genre;
+	private int year;
+	private boolean isAFavorite;
+	private Integer rating;
+
+    public Song(String title, String artist, String album, String genre, int year) {
+        this.title = title;
+        this.artist = artist;
+        this.album = album;
+        this.genre = genre;
+        this.year = year;
+        this.isAFavorite = false; // this will switch to true if it is checked as a favorite
+        this.rating = null; // this will be null until/ if the song is rated 
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public boolean isFavorite() {
+        return isAFavorite;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setFavorite(boolean favorite) {
+    	isAFavorite = favorite;
+    }
+
+    public void setRating(Integer rating) {
+    	// rating has to be between 1 and 5
+        if (rating != null && rating >= 1 && rating <= 5) {
+            this.rating = rating;
+            if (rating == 5) {
+                this.isAFavorite = true;
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return title + " by " + artist + " from album " + album;
+    }
+}
+
+
